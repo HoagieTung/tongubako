09929b39714aa47c2c867967fa8116a85501e66a
HoagieTung <homo.ignotus@outlook.com>
2024-04-28 21:49:15 +0100

    123

---
+++ tongubako/kalman_filter/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 28 13:46:41 2024
+
+@author: Hogan
+"""
+
--- tongubako/kalman_filter/main.py
+++ tongubako/kalman_filter/main.py
@@ -13,3 +13,26 @@ from statsmodels.tsa.ar_model import AR
 class KalmanFilter:
     def __init__(self):
         return
+
+
+
+def update_one_step(x, z, P, F, H, R, Q=None, B=None, u=None):
+    
+    # Predict Step
+    xNextPred = F.dot(x) if B is None else F.dot(x) + B.dot(u)
+    pNextPred = F.dot(P).dot(F.T) if Q is None else F.dot(P).dot(F.T) + Q
+    
+    # Update Step
+    K = P.dot(H.T).dot(np.linalg.inv(H.dot(P).dot(H.T)+R))
+    xNextUpdt = xNextPred + (K.T).dot(z-H.dot(xNextPred))
+    pNextUpdt = pNextPred - (K.T).dot(H).dot(pNextPred)
+    
+    result = {
+        'xPredicted':xNextPred,
+        'PPredicted':pNextPred,
+        'xUpdated':xNextUpdt,
+        'PUpdated':pNextUpdt,
+        'K':K,   
+        }
+    
+    return result
