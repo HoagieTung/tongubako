b44c32d474386d8edb7ca5bfc69495cc0bcd131f
HoagieTung <homo.ignotus@outlook.com>
2024-05-25 18:28:06 +0100

    Create main.py

---
+++ tongubako/htsql/main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 14:19:08 2024
+
+@author: homoi
+"""
+
+class SQL():
+    def __init__(self, schema, dbtype='postgres'):
+        self.dbtype = dbtype
+        self.schema = schema
+    
+    def connect(self):
+        
+        return
+    
+    def insert_dataframe_to_table(self, df, table, delete_clause=None):
+        
+        return
+        
+    def generate_delete_clause(self, columns):
+        if isinstance(columns, str):
+            columns = [columns]
+       
+        return
+        
+        
+        
