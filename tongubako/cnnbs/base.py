36353b038a3a9d49edb3cd305914b5e7451c9021
HoagieTung <homo.ignotus@outlook.com>
2024-07-13 10:09:18 +0100

    123

--- tongubako/cnnbs/base.py
+++ tongubako/cnnbs/base.py
@@ -20,8 +20,16 @@ class CNNBS():
         self.data_dictionary = self.data_list.set_index('SID').to_dict(orient='index')
         return
     
+    def search_category_sid(self, name):
+        
+        return
+    
     def get_series_data(self, sid, bound_type='last', start_date=None, end_date=None, details=True):
         
         return
     
+    def get_category_data(self, category_id, freq='M', bound_type='last', start_date=None, end_date=None, details=True):
+        raw_data = fetch_data.fetch_category_data(category_id, freq=freq, period="1990-", proxies=self.proxies)
+        cleaned_data = process_data.process_series_data(raw_data, freq, bound_type)
+        return cleaned_data
 
--- tongubako/cnnbs/fetch_data.py
+++ tongubako/cnnbs/fetch_data.py
@@ -7,14 +7,11 @@ Created on Tue Jul  9 23:06:12 2024
 import time
 from functools import lru_cache
 from typing import Union, Literal, List, Dict
-
 import numpy as np
 import pandas as pd
 import requests
 import urllib3
 from urllib3.exceptions import InsecureRequestWarning
-
-# 忽略InsecureRequestWarning警告
 urllib3.disable_warnings(InsecureRequestWarning)
 
 db_code = {"M": "hgyd", "Q": "hgjd", "A": "hgnd"}
@@ -28,36 +25,9 @@ def get_child_indicators(category_id: str, freq: str, proxies=None) -> List[Dict
 
 test = get_child_indicators('A0102','M')
 
-def _get_nbs_wds_tree(idcode: str, dbcode: str, rowcode: str) -> List[Dict]:
-    """
-    获取地区数据的可选指标目录树
-    :param idcode: 指标编码
-    :param dbcode: 库编码
-    :param rowcode: 值为zb是返回地区的编码，值为reg时返回可选指标的编码
-    :return:  json数据
-    """
+
+def fetch_category_data(category_id, freq='M', period="1995-", proxies=None):
     url = "https://data.stats.gov.cn/english/easyquery.htm"
-    params = {
-        "m": "getOtherWds",
-        "dbcode": dbcode,
-        "rowcode": rowcode,
-        "colcode": "sj",
-        "wds": '[{"wdcode":"zb","valuecode":"%s"}]' % idcode,
-        "k1": str(time.time_ns())[:13],
-    }
-    r = requests.post(url, params=params, verify=False, allow_redirects=True)
-    data_json = r.json()
-    data_json = data_json["returndata"][0]["nodes"]
-    return data_json
-
-
-
-
-def fetch_data(sid, freq='M', period="1990-", proxies=None):
-    url = "https://data.stats.gov.cn/english/easyquery.htm"
-    sid = 'A010201'
-    period="1990-"
-    freq='M'
     params = {
         "m": "QueryData",
         "dbcode": db_code[freq],
@@ -65,38 +35,11 @@ def fetch_data(sid, freq='M', period="1990-", proxies=None):
         "colcode": "sj",
         "wds": "[]",
         "dfwds": '[{"wdcode":"zb","valuecode":"%s"}, '
-        '{"wdcode":"sj","valuecode":"%s"}]' % (sid, period),
+        '{"wdcode":"sj","valuecode":"%s"}]' % (category_id, period),
         "k1": str(time.time_ns())[:13],
     }
     r = requests.get(url, params=params, verify=False, allow_redirects=True)
     data_json = r.json()
 
-    # 整理为dataframe
-    temp_df = pd.DataFrame(data_json["returndata"]["datanodes"])
-    temp_df["data"] = temp_df["data"].apply(
-        lambda x: x["data"] if x["hasdata"] else None
-    )
-
-    wdnodes = data_json["returndata"]["wdnodes"]
-    wn_df_list = []
-    for wn in wdnodes:
-        wn_df_list.append(
-            pd.DataFrame(wn["nodes"])
-            .assign(
-                funit=lambda df: df["unit"].apply(lambda x: "(" + x + ")" if x else x)
-            )
-            .assign(fname=lambda df: df["cname"] + df["funit"]),
-        )
-
-    row_name, column_name = (
-        wn_df_list[0]["fname"],
-        wn_df_list[1]["fname"],
-    )
-
-    data_ndarray = np.reshape(temp_df["data"], (len(row_name), len(column_name)))
-    data_df = pd.DataFrame(data=data_ndarray, columns=column_name, index=row_name).T
-    data_df.index.name = None
-    data_df.columns.name = None
-
-    return
+    return data_json
 
